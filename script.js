b6f6af12d05f7dececa1ef736d09435b144ca8fd
Suniel <sunieljolly@googlemail.com>
2022-05-05 16:53:39 +0700

    05-05-2022

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('sticky rice for life')
+BASE_URL = "https://stickyricesoftware.github.io/";
+console.log("sticky rice for life");
+
+projectsArray = [BASE_URL + "matchmatch", BASE_URL + "projectone"];
+
+const projects = document.getElementById("projects");
+
+projectsArray.forEach((item) => {
+    const button = document.createElement("button");
+    button.innerText = item.slice(37);
+    projects.appendChild(button);
+    button.addEventListener("click", () => {
+      window.location.href = item
+    });        
+}) ;
+
+
