330984a0a3221eb35d49144b4404b4c7b70d59db
Suniel <sunieljolly@googlemail.com>
2022-05-07 18:15:27 +0700

    translation changes

--- bbcmundo/bbcmundo.js
+++ bbcmundo/bbcmundo.js
@@ -1,41 +1,97 @@
-console.error("BBC NEWS | Mundo");
-var API_KEY = config.API_KEY;
+console.warn("BBC NEWS | Mundo");
+var articleNumber = 0;
+const counter = document.getElementById('counter')
+
 const options = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "bbc-news-mundo.p.rapidapi.com",
-    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Key": "5326a3a741msh8a7fd584a716b8bp10e0cejsn223b2338e2fe",
   },
 };
-
+var data;
 fetch("https://bbc-news-mundo.p.rapidapi.com/news", options)
   .then((response) => response.json())
-  .then((response) => loadNews(response))
+  .then((response) => {
+    loadArticle(response, articleNumber);
+    data = response;
+    gtag("event", "User", {
+      "Loaded news articles:": data.length,
+    });
+    console.log(data)
+    const loader = document.getElementById("loader");
+    loader.remove();
+    
+  })
   .catch((err) => console.error(err));
 
+function loadArticle(articles, number) {
+  const summary = document.getElementById('summary')
+  summary.innerText = articles[number].summary;
+  const headline = document.getElementById('headline')
+  headline.innerText = articles[number].title;  
+  const link = document.getElementById('link') 
+  const url = articles[number].link;
+  link.setAttribute('href', url)
+  counter.innerHTML = articleNumber+1 + '/' + articles.length
+
+  const copyHeadline = document.createElement("button");
+  copyHeadline.classList.add('copy')
+  const headlineText = document.createTextNode("Copy"); 
+  copyHeadline.appendChild(headlineText);
+  document.getElementById('headline').appendChild(copyHeadline)
+  copyHeadline.addEventListener("click", () => {
+    selectText('headline')
+  });
+  const copySummary = document.createElement("button");
+  copySummary.classList.add('copy')
+  const summaryText = document.createTextNode("Copy"); 
+  copySummary.appendChild(summaryText);
+  document.getElementById('summary').appendChild(copySummary)
+  copySummary.addEventListener("click", () => {
+    selectText('summary')
+  });
+}
+
+let next = document.getElementById("next");
+let previous = document.getElementById("previous");
+let copyButton = document.getElementById("copy");
+
+next.addEventListener("click", () => {
+  if (articleNumber < data.length) { articleNumber++;}
+  if (articleNumber == data.length) { articleNumber--;}
+  loadArticle(data, articleNumber);
+
+});
+
+previous.addEventListener("click", () => {
+  if (articleNumber > 0) {
+    articleNumber--;
+  }
+  loadArticle(data, articleNumber);
+
+});
+
+function selectText(element) {
+  var doc = document
+      , text = doc.getElementById(element)
+      , range, selection
+  ;    
+  if (doc.body.createTextRange) { //ms
+      range = doc.body.createTextRange();
+      range.moveToElementText(text);
+      range.select();
+  } else if (window.getSelection) { //all others
+      selection = window.getSelection();        
+      range = doc.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+  }
 
 
-function loadNews(articles) {
-  articles.forEach((element) => {
-    if (1 == 1) {
-      const div = document.createElement("div");
-      div.classList.add("article");
-      const summary = document.createElement("p");
-      summary.innerText = element.summary;
-      summary.classList.add("summary");
-      const a = document.createElement("a");
-      const linkText = document.createTextNode(element.title);
-      a.classList.add("headline");
-      a.appendChild(linkText);
-      a.href = element.link;
-      div.appendChild(a);
-      div.appendChild(summary);
-      document.body.appendChild(div);
-    }
-  });
-  const element = document.getElementById('loader');
-  element.remove();
-  gtag('event', 'User', {
-    'Loaded news articles:' : articles.length
-  });
+}
+
+if(dispatchEvent){
+  console.log('Here')
 }
