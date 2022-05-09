bb9a171370149e5cbbdc7d3295c1cfa941dc1c66
Suniel <sunieljolly@googlemail.com>
2022-05-09 17:16:35 +0700

    optimised google translate

--- bbcmundo/bbcmundo.js
+++ bbcmundo/bbcmundo.js
@@ -1,6 +1,6 @@
 //console.warn("BBC NEWS | Mundo");
 var articleNumber = 0;
-const counter = document.getElementById('counter')
+const counter = document.getElementById("counter");
 
 const options = {
   method: "GET",
@@ -18,34 +18,35 @@ fetch("https://bbc-news-mundo.p.rapidapi.com/news", options)
     gtag("event", "User", {
       "Loaded news articles:": data.length,
     });
-    console.log(data)
+    console.log(data);
     const loader = document.getElementById("loader");
     loader.remove();
-    
   })
   .catch((err) => console.error(err));
 
 function loadArticle(articles, number) {
-  const summary = document.getElementById('summary')
+
+  const articleEN = document.getElementById('articleEN')
+  articleEN.classList.add('hide')
+
+  const summary = document.getElementById("summary");
   summary.innerText = articles[number].summary;
-  const summaryEN = document.getElementById('summaryEN')
+  const summaryEN = document.getElementById("summaryEN");
   summaryEN.innerText = articles[number].summary;
 
-  const headline = document.getElementById('headline')
+  const headline = document.getElementById("headline");
   headline.innerText = articles[number].title;
-  
-  const headlineEN = document.getElementById('headlineEN')
+
+  const headlineEN = document.getElementById("headlineEN");
   headlineEN.innerText = articles[number].title;
 
-  const time = document.getElementById('time')
+  const time = document.getElementById("time");
   time.innerText = articles[number].timeText;
 
-  const link = document.getElementById('link') 
+  const link = document.getElementById("link");
   const url = articles[number].link;
-  link.setAttribute('href', url)
-  counter.innerHTML = articleNumber+1 + '/' + articles.length
-
-
+  link.setAttribute("href", url);
+  counter.innerHTML = articleNumber + 1 + "/" + articles.length;
 }
 
 let next = document.getElementById("next");
@@ -53,24 +54,42 @@ let previous = document.getElementById("previous");
 let copyButton = document.getElementById("copy");
 
 next.addEventListener("click", () => {
-  if (articleNumber < data.length) { articleNumber++;}
-  if (articleNumber == data.length) { articleNumber--;}
+  if (articleNumber < data.length) {
+    articleNumber++;
+  }
+  if (articleNumber == data.length) {
+    articleNumber--;
+  }
   loadArticle(data, articleNumber);
 });
 
 previous.addEventListener("click", () => {
-  if (articleNumber > 0) { articleNumber--; }
+  if (articleNumber > 0) {
+    articleNumber--;
+  }
   loadArticle(data, articleNumber);
 });
 
-
-
-
+setTimeout(() => {
+  let test = document.querySelectorAll(".goog-logo-link");
+  test[0].remove();
+}, 1500);
 
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'es',
-    includedLanguages: 'en',
-    layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
-  }, 'google_translate_element');
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "es",
+      includedLanguages: "en",
+      layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
+    },
+    "google_translate_element"
+  );
 }
+
+
+let selectLanguage = document.getElementById("google_translate_element");
+console.log(selectLanguage)
+selectLanguage.addEventListener("click", () => {
+  article = document.getElementById('articleEN')
+  article.classList.remove('hide')
+});
