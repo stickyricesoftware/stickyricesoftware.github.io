2548d47687b0dc9ac8f897e045c8147f06e9875c
sunieljolly <86339013+sunieljolly@users.noreply.github.com>
2022-09-25 15:49:20 +0800

    skr-collection

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 BASE_URL = "https://stickyricesoftware.github.io/";
 console.log("sticky rice for life");
 
-projectsArray = [BASE_URL + "matchmatch", BASE_URL + "bbcmundo", BASE_URL + "skr-collection/skr/skr-home", BASE_URL + "skr-collection/skr1/skr1-home",];
+projectsArray = [BASE_URL + "matchmatch", BASE_URL + "bbcmundo", BASE_URL + "skr-collection/skr/skr-home", BASE_URL + "skr-collection/skr1/skr1-home", BASE_URL + "skr-collection/skr2/skr2-home",];
 
 const projects = document.getElementById("projects");
 
