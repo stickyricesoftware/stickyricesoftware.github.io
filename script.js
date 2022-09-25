8312fc44c1585762d1a01dd256d10c799e7dffb2
sunieljolly <86339013+sunieljolly@users.noreply.github.com>
2022-09-25 14:39:05 +0800

    skr1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 BASE_URL = "https://stickyricesoftware.github.io/";
 console.log("sticky rice for life");
 
-projectsArray = [BASE_URL + "matchmatch", BASE_URL + "bbcmundo", BASE_URL + "skr-home",];
+projectsArray = [BASE_URL + "matchmatch", BASE_URL + "bbcmundo", BASE_URL + "skr-home", BASE_URL + "skr1-home",];
 
 const projects = document.getElementById("projects");
 
