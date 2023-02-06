813e4fe4d223203c5c208d4e23bf35a3d4aa7a73
Sunny <sunieljolly@gmail.com>
2023-02-06 11:52:50 +0800

    email log in client

--- email-login-client/index.js
+++ email-login-client/index.js
@@ -1,34 +1,74 @@
-
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-    if (username === "user" && password === "password") {
-        alert("You have successfully logged in.");
-        location.replace("https://stickyricesoftware.github.io/email-login-client/home.html");
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username === "user" && password === "password") {
+    alert("You have successfully logged in.");
+    location.replace(
+      "https://stickyricesoftware.github.io/email-login-client/home.html"
+    );
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
 
 function emailOne() {
- alert('email one')
+  var modal = document.getElementById("email1");
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  modal.style.display = "block";
 }
+
 function emailTwo() {
-  alert('email two')
- }
- function emailThree() {
-  alert('email three')
- }
- function emailFour() {
-  alert('email four')
- }
- function emailFive() {
-  alert('email five')
- }
+  var modal = document.getElementById("email2");
+  var span = document.getElementsByClassName("close")[1];
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  modal.style.display = "block";
+}
+function emailThree() {
+    var modal = document.getElementById("email3");
+    var span = document.getElementsByClassName("close")[2];
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+    modal.style.display = "block";
+}
+function emailFour() {
+    var modal = document.getElementById("email4");
+    var span = document.getElementsByClassName("close")[3];
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+    modal.style.display = "block";
+}
+
 
