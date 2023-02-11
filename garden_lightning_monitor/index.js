2f3a07fa3452eebef381b0d28e169f28bc448566
Sunny <sunieljolly@gmail.com>
2023-02-11 20:55:33 +0800

    lightning

--- garden_lightning_monitor/index.js
+++ garden_lightning_monitor/index.js
@@ -1,4 +1,8 @@
 var xhttp = new XMLHttpRequest();
+const URL = "http://gardenschool.thormobile3.net/SE0148.xml?ms=" + Date.now()
+
+
+
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     myFunction(this);
@@ -6,17 +10,44 @@ xhttp.onreadystatechange = function () {
 };
 xhttp.open(
   "GET",
-  "http://gardenschool.thormobile3.net/SE0148.xml?ms=" + Date.now(),
+  URL,
   true
 );
 xhttp.send();
 
+//
+
+
 function myFunction(xml) {
   console.log(xml.responseXML.all);
-  console.log(xml.responseXML.all[03].innerHTML); //School Name
-  console.log(xml.responseXML.all[23].innerHTML); //ad
-  console.log(xml.responseXML.all[24].innerHTML); //di
-  console.log(xml.responseXML.all[25].innerHTML); //lhl
-  console.log(xml.responseXML.all[26].innerHTML); //fcc
-  console.log(xml.responseXML.all[36].innerHTML); //isDataOld
+  const school = xml.responseXML.all[03].innerHTML;
+  const ad = xml.responseXML.all[23].innerHTML;
+  const di = xml.responseXML.all[24].innerHTML;
+  const lhl = xml.responseXML.all[25].innerHTML;
+  const fcc = xml.responseXML.all[26].innerHTML;
+  const isDataOld = xml.responseXML.all[36].innerHTML;
+
+
+  const para = document.createElement("p");
+  para.innerText = `Name: ${school}
+  Lhl: ${lhl}
+  Di: ${di}
+  Ad: ${ad}
+  Fcc: ${fcc}
+  ${URL}
+  `;
+  document.getElementById("card").appendChild(para);
+
+
+  if (di == 0 ){ document.getElementById("card").style.backgroundColor = "";}
+  if (di > 0 && di < 2.4 ){ document.getElementById("card").style.backgroundColor = "grey";}
+  if (di > 2.3 && di < 3.0 ){ document.getElementById("card").style.backgroundColor = "yellow";}
+  if (di > 2.9 ){ document.getElementById("card").style.backgroundColor = "red";}
+
 }
+
+
+
+
+
+
