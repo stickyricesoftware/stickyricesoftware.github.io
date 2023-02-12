4f16111c50336d158d62655d2cda8bfae2bae836
Sunny <sunieljolly@gmail.com>
2023-02-13 07:49:58 +0800

    UI Design

--- garden-lightning-monitor/index.js
+++ garden-lightning-monitor/index.js
@@ -21,7 +21,9 @@ function myFunction(xml) {
   console.log(xml.responseXML.all);
   const school = xml.responseXML.all[03].innerHTML;
   const ad = xml.responseXML.all[23].innerHTML;
-  const di = xml.responseXML.all[24].innerHTML;
+  //const di = xml.responseXML.all[24].innerHTML;
+  const di = 2.5
+  
   const lhl = xml.responseXML.all[25].innerHTML;
   const fcc = xml.responseXML.all[26].innerHTML;
   const isDataOld = xml.responseXML.all[36].innerHTML;
@@ -32,20 +34,23 @@ function myFunction(xml) {
   document.getElementById("fcc").innerText = fcc;
 
   if (di < 0.1) {
-    document.getElementById("card").style.backgroundColor = "#7CEA9C";
-    document.getElementById("alertText").innerHTML = "ALL CLEAR";
+    document.getElementById("tab-large").style.backgroundColor = "#7CEA9C";
+    document.getElementById("status-text").innerHTML = "ALL CLEAR";
   } //Green
   if (di > 0 && di < 2.4) {
-    document.getElementById("card").style.backgroundColor = "#797D81";
-    document.getElementById("alertText").innerHTML = "CAUTION";
+    document.getElementById("tab-large").style.backgroundColor = "#797D81";
+    document.getElementById("status-text").innerHTML = "CAUTION";
   } //Grey
   if (di > 2.3 && di < 3.0) {
-    document.getElementById("card").style.backgroundColor = "#CBA328";
-    document.getElementById("alertText").innerHTML = "WARNING";
+    document.getElementById("tab-large").style.backgroundColor = "#CBA328";
+    document.getElementById("status-text").innerHTML = "WARNING";
   } //Yellow
   if (di > 2.9) {
-    document.getElementById("card").style.backgroundColor = "#B3001B";
-    document.getElementById("alertText").innerHTML = "RED ALERT";
+    document.getElementById("tab-large").style.backgroundColor = "#B3001B";
+    document.getElementById("status-text").innerHTML = "RED ALERT";
   } //Red
+
+
+  console.log(document.getElementById("status-text"))
 }
 
