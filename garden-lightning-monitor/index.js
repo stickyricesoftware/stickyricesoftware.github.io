d891d2a1557a262a4156610997c788fc24b689e9
Sunny <sunieljolly@gmail.com>
2023-02-12 15:04:56 +0800

    url change

--- garden-lightning-monitor/index.js
+++ garden-lightning-monitor/index.js
@@ -23,24 +23,19 @@ function myFunction(xml) {
   const di = xml.responseXML.all[24].innerHTML;
   const lhl = xml.responseXML.all[25].innerHTML;
   const fcc = xml.responseXML.all[26].innerHTML;
-  const isDataOld = xml.responseXML.all[36].innerHTML;
+  const isDataOld = xml.responseXML.all[36].innerHTML;  
 
 
-  const para = document.createElement("p");
-  para.innerText = `Name: ${school}
-  Lhl: ${lhl}
-  Di: ${di}
-  Ad: ${ad}
-  Fcc: ${fcc}
-  ${URL}
-  `;
-  document.getElementById("card").appendChild(para);
+  document.getElementById("school").innerHTML = school
+  document.getElementById("lhl").innerHTML = lhl
+  document.getElementById("di").innerHTML = di
+  document.getElementById("ad").innerHTML = ad
+  document.getElementById("fcc").innerHTML = fcc
 
-
-  if (di == 0 ){ document.getElementById("card").style.backgroundColor = "";}
-  if (di > 0 && di < 2.4 ){ document.getElementById("card").style.backgroundColor = "grey";}
-  if (di > 2.3 && di < 3.0 ){ document.getElementById("card").style.backgroundColor = "yellow";}
-  if (di > 2.9 ){ document.getElementById("card").style.backgroundColor = "red";}
+  if (di < 0.1 ){ document.getElementById("card").style.backgroundColor = "7CEA9C";} //Green
+  if (di > 0 && di < 2.4 ){ document.getElementById("card").style.backgroundColor = "797D81";} //Grey
+  if (di > 2.3 && di < 3.0 ){ document.getElementById("card").style.backgroundColor = "CBA328";} //Yellow
+  if (di > 2.9 ){ document.getElementById("card").style.backgroundColor = "red";} //Red
 
 }
 
@@ -48,4 +43,4 @@ function myFunction(xml) {
 
 
 
-
+//https://coolors.co/b3001b-7cea9c-cba328-797d81-279af1
