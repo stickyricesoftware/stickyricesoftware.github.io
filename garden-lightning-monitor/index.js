58de6daa6711f245e509b069e26199f0d1fc1fd8
Sunny <sunieljolly@gmail.com>
2023-02-13 07:58:43 +0800

    UI Design

--- garden-lightning-monitor/index.js
+++ garden-lightning-monitor/index.js
@@ -21,8 +21,8 @@ function myFunction(xml) {
   console.log(xml.responseXML.all);
   const school = xml.responseXML.all[03].innerHTML;
   const ad = xml.responseXML.all[23].innerHTML;
-  //const di = xml.responseXML.all[24].innerHTML;
-  const di = 2.5
+  const di = xml.responseXML.all[24].innerHTML;
+
   
   const lhl = xml.responseXML.all[25].innerHTML;
   const fcc = xml.responseXML.all[26].innerHTML;
