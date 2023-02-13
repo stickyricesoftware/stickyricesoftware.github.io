866e6f75273946ade77f60d7fdd450a8c03b6b2c
Sunny <sunieljolly@gmail.com>
2023-02-13 10:18:10 +0800

    UI Design

--- garden-lightning-monitor/index.js
+++ garden-lightning-monitor/index.js
@@ -21,10 +21,12 @@ function myFunction(xml) {
   console.log(xml.responseXML.all);
   const school = xml.responseXML.all[03].innerHTML;
   
-  //const ad = xml.responseXML.all[23].innerHTML;
-  const ad = 8.9
+  const ad = xml.responseXML.all[23].innerHTML;
+  //const ad = 2
   
   const di = xml.responseXML.all[24].innerHTML;  
+  //const di = 3
+  
   const lhl = xml.responseXML.all[25].innerHTML;
   const fcc = xml.responseXML.all[26].innerHTML;
   const isDataOld = xml.responseXML.all[36].innerHTML;
@@ -35,22 +37,28 @@ function myFunction(xml) {
   document.getElementById("fcc").innerText = fcc;
 
   if (di < 0.1) {
-    //document.getElementById("tab-large").style.backgroundColor = "#7CEA9C";
+    //document.getElementById("status-tab").style.background = "#7CEA9C";
     document.getElementById("status-text").innerHTML = "ALL CLEAR";
   } //Green
   if (di > 0 && di < 2.4) {
-    document.getElementById("tab-large").style.backgroundColor = "#797D81";
+    document.getElementById("status-tab").style.background = "#797D81";
     document.getElementById("status-text").innerHTML = "CAUTION";
+    
   } //Grey
   if (di > 2.3 && di < 3.0) {
-    document.getElementById("tab-large").style.backgroundColor = "#CBA328";
+    document.getElementById("status-tab").style.background = "#CBA328";
     document.getElementById("status-text").innerHTML = "WARNING";
   } //Yellow
   if (di > 2.9) {
-    document.getElementById("tab-large").style.background = "#B3001B";
+    document.getElementById("status-tab").style.background = "#B3001B";
     document.getElementById("status-text").innerHTML = "RED ALERT";
   } //Red
-
+  if (ad > 0){
+    document.getElementById("bar-text").innerHTML = 'Time since last discharge/lightning strike. Will count down from 10. At zero, an "All Clear" will be signaled';
+  }
   document.getElementById("bar-data").style.width = (ad*10) + "%"
+
   
 }
+
+//
