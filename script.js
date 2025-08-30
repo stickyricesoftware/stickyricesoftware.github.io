26eb6d062a237181ab8b56748d2b786e721674c9
Sunny <sunieljolly@googlemail.com>
2025-08-30 08:40:21 +0800

    GIS-FPL

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ projectsArray = [BASE_URL + "matchmatch", BASE_URL + "bbcmundo"];
 
 const projects = document.getElementById("projects");
 
-// projectsArray.forEach((item) => {
-//     const button = document.createElement("button");
-//     button.innerText = item.slice(37);
-//     projects.appendChild(button);
-//     button.addEventListener("click", () => {
-//       window.location.href = item
-//     });        
-// }) ;
+
 
 
